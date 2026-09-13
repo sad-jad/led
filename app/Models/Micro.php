1f5f982f4316a7796adeffe8ecd8a00a7ef65171
Amir <amir@example.com>
2026-09-13 02:39:23 -1200

    تکمیل مدل میکرو و افزودن نام به فیلدهای قابل ثبت

--- app/Models/Micro.php
+++ app/Models/Micro.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Micro extends Model
 {
+    protected $fillable = [
+        'name',
+    ];
+
     public function boards(): HasMany
     {
         return $this->hasMany(Board::class);
